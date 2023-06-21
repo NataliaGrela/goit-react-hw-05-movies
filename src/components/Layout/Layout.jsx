d9bad0b2b2773e741c2729a977ab04885210bff9
Natalia Grela <nataliagrela83@mail.com>
2023-06-21 20:09:24 +0200

    fixed

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,30 +1,42 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from 'react-router-dom';
 
 const Layout = () => {
-    const baseUrl = 'goit-react-hw-05-movies';
-    return (
-      <div>
-        <header>
-          <nav>
-            <ul className="nav">
-              <li>
-                <Link className="nav-el" to={`${baseUrl}/`}>
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link className="nav-el" to={`${baseUrl}/movies`}>
-                  Movies
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        <main>
-          <Outlet></Outlet>
-        </main>
-      </div>
-    );
+  const baseUrl = 'goit-react-hw-05-movies';
+  const location = useLocation();
+  console.log(location);
+  const isActive = page => {
+    return location.pathname.includes(page);
+  };
+
+  return (
+    <div>
+      <header>
+        <nav>
+          <ul className="nav">
+            <li>
+              <Link
+                className={!isActive('/movies') ? 'nav-el active' : 'nav-el'}
+                to={`${baseUrl}/`}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                className={isActive('/movies') ? 'nav-el active' : 'nav-el'}
+                to={`${baseUrl}/movies`}
+              >
+                Movies
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Outlet></Outlet>
+      </main>
+    </div>
+  );
 };
 
 export default Layout;
